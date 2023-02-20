c6e1e32813810bf6dd34916571168f4b75432cd8
TimD <timothee.diroian@gmail.com>
2023-02-20 14:43:24 +0100

    Creation of a placement function to create and place the word inside the grid game

--- src/js/Puzzle.js
+++ src/js/Puzzle.js
@@ -12,3 +12,121 @@ export function gridCreator (puzzleSize){
 
     return gameGrid;
 };
+
+
+const gameGrid6 = gridCreator(6);
+const wordList = ['cat', 'dog', 'tall']
+
+export function wordPlacement(wordList, gameGrid){
+    const direction = ['vertical', 'horizontal', 'inverseVertical', 'inverseHorizontal', 'diagonalUpRigth', 'diagonalUpLeft', 'diagonalDownRigth', 'diagonalDownLeft'];
+    const wordInfo = [];
+    for (let i = 0; i < wordList.length; i++) {
+        wordInfo[i] = {
+            word: wordList[i].toUpperCase(), 
+            direction : direction[Math.floor(Math.random()* direction.length)],
+            position: [],
+        };
+    };
+
+    const placement = (wInfo) =>{
+        const randomMinMax = (min, max) => Math.floor(Math.random()*(max - min +1)+ min);
+        let line;
+        let column;
+        switch (wInfo.direction){
+            case 'vertical':
+                line = Math.floor(Math.random()*gameGrid.length-1);
+                column = randomMinMax(0, (gameGrid.length - wInfo.word.length));
+                for (let i = 0; i < wInfo.word.length; i++) {
+                    wInfo.position[i] = {
+                        line: line,
+                        column: column + i,
+                    }
+                };                
+                break;
+            case 'horizontal' :
+                line = randomMinMax(0, (gameGrid.length - wInfo.word.length));
+                column = Math.floor(Math.random()*gameGrid.length-1);
+                for (let i = 0; i < wInfo.word.length; i++) {
+                    wInfo.position[i] = {
+                        line: line + i,
+                        column: column,
+                    }
+                };                
+                break;
+            case 'inverseVertical' :
+                line = Math.floor(Math.random()*gameGrid.length-1);
+                column = randomMinMax((gameGrid.length - wInfo.word.length + 1), gameGrid.length -1);
+                for (let i = 0; i < wInfo.word.length; i++) {
+                    wInfo.position[i] = {
+                        line: line,
+                        column: column - i,
+                    }
+                };                
+                break;
+            case 'inverseHorizontal' :
+                line = randomMinMax((gameGrid.length - wInfo.word.length + 1), gameGrid.length -1);
+                column = Math.floor(Math.random()*gameGrid.length-1);
+                for (let i = 0; i < wInfo.word.length; i++) {
+                    wInfo.position[i] = {
+                        line: line -i,
+                        column: column,
+                    }
+                };                
+                break;
+            case  'diagonalDownRigth':
+                line = randomMinMax(0, (gameGrid.length - wInfo.word.length));
+                column = randomMinMax(0, (gameGrid.length - wInfo.word.length));
+                for (let i = 0; i < wInfo.word.length; i++) {
+                    wInfo.position[i] = {
+                        line: line +i,
+                        column: column+i,
+                    }
+                };                
+                break;
+            case 'diagonalDownLeft' :
+                line = randomMinMax(0, (gameGrid.length - wInfo.word.length));
+                column = randomMinMax((gameGrid.length - wInfo.word.length + 1), gameGrid.length-1);
+                for (let i = 0; i < wInfo.word.length; i++) {
+                    wInfo.position[i] = {
+                        line: line +i,
+                        column: column-i,
+                    }
+                };                
+                break;
+            case 'diagonalUpRigth' :
+                line = randomMinMax((gameGrid.length - wInfo.word.length + 1), gameGrid.length -1);
+                column = randomMinMax(0, (gameGrid.length - wInfo.word.length));
+                for (let i = 0; i < wInfo.word.length; i++) {
+                    wInfo.position[i] = {
+                        line: line -i,
+                        column: column+i,
+                    }
+                };                
+                break;
+            case 'diagonalUpLeft' :
+                line = randomMinMax((gameGrid.length - wInfo.word.length + 1), gameGrid.length - 1);
+                column = randomMinMax((gameGrid.length - wInfo.word.length + 1), gameGrid.length - 1);
+                for (let i = 0; i < wInfo.word.length; i++) {
+                    wInfo.position[i] = {
+                        line: line -i,
+                        column: column-i,
+                    }
+                };                
+                break;
+            default:
+            return;
+        }
+    }
+
+    for (let i = 0; i < wordInfo.length; i++) {
+        placement(wordInfo[i])
+    };
+
+    
+
+    console.log(wordInfo)
+
+    return wordInfo;
+};
+
+wordPlacement(wordList, gameGrid6);
--- src/js/Puzzle.test.js
+++ src/js/Puzzle.test.js
@@ -1,27 +1,40 @@
 import { describe, expect, test } from '@jest/globals';
-import { gridCreator } from './Puzzle';
+import { gridCreator, wordPlacement } from './Puzzle';
 
 describe("Test all the function that compose the puzzle function", () =>{
-    // test of the gridCreator function
     describe("gridCreator function", () =>{
-        //define parametre of the test
-        const size = 16;
+        const size = 6;
         const result = gridCreator(size);
         const character = 'ACBDEFGHIJKLMNOPQRSTUVWXYZ';
         test("With the size parametre the function should give us an array of array. they length must match the size parameter.", function(){
-            expect(size).toStrictEqual(result.length);
+            expect(result).toHaveLength(size);
             result.forEach(lineArray =>{
-                // we look inside the main array each array must be the same size
-                expect(lineArray.length).toStrictEqual(size);
+                expect(lineArray).toHaveLength(size);
             });
         });
         test("The lineArray inside the main array must be fill with uppercase character [A-Z] elements", ()=>{
             result.forEach(lineArray =>{
                 lineArray.forEach(element =>{
-                    // we look inside the main array and now inside each element of the LineArray they must be an uppercase letter
                     expect(character.includes(element)).toBeTruthy();
                 })
             })
         })
     });
+    describe('wordPlacement function', () =>{
+        const wordList = ['cat', 'dog', 'tall'];
+        const gridArr = new Array(6).fill(new Array(6).fill('A'));
+        const direction = ['vertical', 'horizontal', 'inverseVertical', 'inverseHorizontal', 'diagonalUpRigth', 'diagonalUpLeft', 'diagonalDownRigth', 'diagonalDownLeft'];
+        const posPossibility = [0, 1, 2, 3, 4, 5];
+        const result = wordPlacement(wordList, gridArr)
+        test('It should return an array of object for each word in the wordList parameter:', () =>{
+            expect(result).toHaveLength(wordList.length);
+        });
+        test('Each object in the Array must have key of word, direction, position', () =>{
+            result.forEach(word =>{
+                expect(word).toHaveProperty('word');
+                expect(word).toHaveProperty('direction');
+                expect(word).toHaveProperty('position');
+            });
+        })
+    })
 });
